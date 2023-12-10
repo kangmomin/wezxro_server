e9b92044ec088a8b8701e1b03d2c664898181ddc
koldin108902 <alex108902@naver.com>
2023-12-11 06:28:18 +0900

    Feat: validation 디펜선시 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 
 	// JWT
 	implementation ("io.jsonwebtoken:jjwt-api:0.11.2")
