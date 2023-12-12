a61ecbd4452c3dec2f5930c918daf13ff94fbc0c
koldin108902 <alex108902@naver.com>
2023-12-13 06:05:45 +0900

    Feat: queryDSL을 repository와 분리

--- src/main/kotlin/com/hwalaon/wezxro_server/domain/account/persistence/AccountPersistenceAdapter.kt
+++ src/main/kotlin/com/hwalaon/wezxro_server/domain/account/persistence/AccountPersistenceAdapter.kt
@@ -2,18 +2,28 @@ package com.hwalaon.wezxro_server.domain.account.persistence
 
 import com.hwalaon.wezxro_server.domain.account.controller.request.LoginRequest
 import com.hwalaon.wezxro_server.domain.account.exception.AccountNotFoundException
+import com.hwalaon.wezxro_server.domain.account.model.Account
 import com.hwalaon.wezxro_server.domain.account.persistence.mapper.AccountMapper
 import com.hwalaon.wezxro_server.domain.account.persistence.repository.AccountEntityRepository
+import com.hwalaon.wezxro_server.domain.account.persistence.repository.custom.AccountEntityRepositoryCustom
 import org.springframework.stereotype.Component
 
 @Component
 class AccountPersistenceAdapter(
     private val accountEntityRepository: AccountEntityRepository,
-    private val accountMapper: AccountMapper
+    private val accountMapper: AccountMapper,
+    private val accountEntityRepositoryCustom: AccountEntityRepositoryCustom
 ) {
     fun login(loginRequest: LoginRequest) =
         accountEntityRepository.findOneByEmail(loginRequest.email).let {
             if (it == null) throw AccountNotFoundException()
             accountMapper.toDomain(it)
         }
+
+    fun join(account: Account) =
+        accountMapper.toEntity(account).let {
+            accountEntityRepository.save(it)
+        }
+
+    fun isExistAccount(email: String) = accountEntityRepositoryCustom.isExistAccount(email)
 }
--- src/main/kotlin/com/hwalaon/wezxro_server/domain/account/persistence/repository/AccountEntityRepository.kt
+++ src/main/kotlin/com/hwalaon/wezxro_server/domain/account/persistence/repository/AccountEntityRepository.kt
@@ -1,12 +1,11 @@
 package com.hwalaon.wezxro_server.domain.account.persistence.repository
 
 import com.hwalaon.wezxro_server.domain.account.persistence.entity.AccountEntity
-import com.hwalaon.wezxro_server.domain.account.persistence.repository.custom.AccountEntityRepositoryCustom
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface AccountEntityRepository: JpaRepository<AccountEntity, Int>, AccountEntityRepositoryCustom {
+interface AccountEntityRepository: JpaRepository<AccountEntity, Int> {
 
     fun findOneByEmail(email: String): AccountEntity?
 }
