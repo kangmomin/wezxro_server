9900dd79adfee9983a8b15b084533da1ca73334a
koldin108902 <alex108902@naver.com>
2024-01-08 22:46:18 +0900

    Update: 데이터를 찾지 못하였을 때 404 -> 204

--- src/main/kotlin/com/hwalaon/wezxro_server/global/exception/ErrorCode.kt
+++ src/main/kotlin/com/hwalaon/wezxro_server/global/exception/ErrorCode.kt
@@ -8,10 +8,10 @@ enum class ErrorCode(
 ) {
     FORBIDDEN_ERROR("접근 권한이 없습니다.", HttpStatus.FORBIDDEN),
     UNAUTHORIZED_ERROR("로그인이 필요한 작업입니다.", HttpStatus.UNAUTHORIZED),
-    ACCOUNT_NOT_FOUND_ERROR("계정을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    ACCOUNT_NOT_FOUND_ERROR("계정을 찾을 수 없습니다.", HttpStatus.NO_CONTENT),
     UNEXPECTED_ERROR("서버에서 오류가 발생하였습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
     NON_BODY_ERROR("데이터가 정상적으로 들어오지 않았습니다.", HttpStatus.BAD_REQUEST),
     NOT_ENOUGH_DATA_ERROR("필수 요청 데이터가 정상적으로 입력되지 않았습니다.", HttpStatus.BAD_REQUEST),
     ACCOUNT_ALREADY_JOINED_ERROR("해당 이메일은 이미 가입이 완료되었습니다.", HttpStatus.CONFLICT),
-    REFRESH_TOKEN_NOT_FOUND("로그인 정보를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    REFRESH_TOKEN_NOT_FOUND("로그인 정보를 찾을 수 없습니다.", HttpStatus.NO_CONTENT),
 }
