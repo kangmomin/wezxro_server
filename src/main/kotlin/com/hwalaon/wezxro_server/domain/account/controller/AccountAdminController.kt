e6ba02cd12fcd764c2a094ca3f2a8a53a491cb83
koldin108902 <alex108902@naver.com>
2024-02-06 04:49:32 +0900

    Refector: 어드민 기능 컨트롤러 단에서 분리

--- src/main/kotlin/com/hwalaon/wezxro_server/domain/account/controller/AccountAdminController.kt
+++ src/main/kotlin/com/hwalaon/wezxro_server/domain/account/controller/AccountAdminController.kt
@@ -1,31 +1,32 @@
 package com.hwalaon.wezxro_server.domain.account.controller
 
-import com.hwalaon.wezxro_server.domain.account.controller.request.JoinRequest
-import com.hwalaon.wezxro_server.domain.account.controller.request.LoginRequest
+import com.hwalaon.wezxro_server.domain.account.controller.request.UpdateAccountRequest
 import com.hwalaon.wezxro_server.domain.account.controller.response.AccountDetailResponse
 import com.hwalaon.wezxro_server.domain.account.service.CommandAccountService
 import com.hwalaon.wezxro_server.domain.account.service.QueryAccountService
 import com.hwalaon.wezxro_server.global.common.basic.response.BasicResponse
-import com.hwalaon.wezxro_server.global.common.basic.response.MsgResponse
 import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/u")
-class AccountController(
+@RequestMapping("/u/admin")
+class AccountAdminController(
     private val queryAccountService: QueryAccountService,
     private val commandAccountService: CommandAccountService,
 ) {
 
-    @PostMapping("/login")
-    fun login(@RequestBody @Valid loginRequest: LoginRequest) =
-        BasicResponse.ok(queryAccountService.login(loginRequest))
-
-    @PostMapping("/join")
-    fun join(@RequestBody @Valid joinRequest: JoinRequest) =
-        commandAccountService.join(joinRequest).run {
-            BasicResponse.created(MsgResponse("회원가입에 성공하였습니다."))
+    @PatchMapping("/update")
+    fun updateInfo(@RequestBody @Valid updateAccountRequest: UpdateAccountRequest) =
+        commandAccountService.updateAccountInfo(updateAccountRequest.toDomain()).run {
+            BasicResponse.okMsg("계정 정보를 성공적으로 변경하였습니다.")
         }
+
+    @DeleteMapping("/delete/{id}")
+    fun delete(@PathVariable("id") id: Int) =
+        commandAccountService.deleteAccount(id).run {
+            BasicResponse.okMsg("삭제되었습니다.")
+        }
+
 
     @PostMapping("/detail/{id}")
     fun accountDetails(@PathVariable("id") id: Int) =
