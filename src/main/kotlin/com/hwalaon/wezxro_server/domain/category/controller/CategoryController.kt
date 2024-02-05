2010e990b1cb89d439681ad540ec48d711a2b5e4
koldin108902 <alex108902@naver.com>
2024-02-06 05:14:33 +0900

    Refector: admin 역할 분활

--- src/main/kotlin/com/hwalaon/wezxro_server/domain/category/controller/CategoryController.kt
+++ src/main/kotlin/com/hwalaon/wezxro_server/domain/category/controller/CategoryController.kt
@@ -1,19 +1,15 @@
 package com.hwalaon.wezxro_server.domain.category.controller
 
-import com.hwalaon.wezxro_server.domain.category.controller.request.SaveCategoryRequest
-import com.hwalaon.wezxro_server.domain.category.service.CommandCategoryService
 import com.hwalaon.wezxro_server.domain.category.service.QueryCategoryService
 import com.hwalaon.wezxro_server.global.common.basic.response.BasicResponse
 import com.hwalaon.wezxro_server.global.security.principal.PrincipalDetails
-import jakarta.validation.Valid
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 
 @RequestMapping("/c")
 @RestController
 class CategoryController(
-    private val queryCategoryService: QueryCategoryService,
-    private val commandCategoryService: CommandCategoryService
+    private val queryCategoryService: QueryCategoryService
 ) {
 
     @GetMapping("/list")
@@ -21,25 +17,4 @@ class CategoryController(
         @AuthenticationPrincipal userInfo: PrincipalDetails) =
         BasicResponse.ok(
             queryCategoryService.categoryList(userInfo.account.clientId!!))
-
-    @PostMapping("/add")
-    fun categoryAdd(@RequestBody @Valid categoryRequest: SaveCategoryRequest) =
-        BasicResponse.ok(
-            commandCategoryService.addCategory(
-                categoryRequest.toDomain()))
-
-    @PostMapping("/delete/{id}")
-    fun categoryDelete(@PathVariable("id") id: Long) =
-        commandCategoryService.delete(id).run {
-            BasicResponse.okMsg("서비스를 삭제하였습니다.")
-        }
-
-    @PostMapping("/update/{id}")
-    fun categoryUpdate(@RequestBody @Valid categoryRequest: SaveCategoryRequest,
-                       @PathVariable("id") categoryId: Long) =
-        commandCategoryService.updateCategory(
-            categoryId,
-            categoryRequest.toDomain()).run {
-                BasicResponse.okMsg("서비스를 수정하였습니다.")
-        }
 }
