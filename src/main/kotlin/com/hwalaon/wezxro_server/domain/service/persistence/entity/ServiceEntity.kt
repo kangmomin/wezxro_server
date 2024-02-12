9cf110dec6cdcab450828998d46c2a62cadafa97
koldin108902 <alex108902@naver.com>
2024-02-12 09:19:00 +0900

    Fix: Service 엔티티 제약조건 설정 및 Enum 설정

--- src/main/kotlin/com/hwalaon/wezxro_server/domain/service/persistence/entity/ServiceEntity.kt
+++ src/main/kotlin/com/hwalaon/wezxro_server/domain/service/persistence/entity/ServiceEntity.kt
@@ -11,16 +11,39 @@ class ServiceEntity (
     @Id @Column(name = "service_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Int?,
+
+    @Column(nullable = false)
     var clientId: UUID?,
+
+    @Column(nullable = false)
     var providerId: Int?,
+    @Column(nullable = false)
     var categoryId: Int?,
+
+    @Column(nullable = false)
     var apiServiceId: Int?,
+
+    @Column(nullable = false)
     var name: String?,
+    @Column(nullable = false)
     var type: String,
+
+    @Column(nullable = false)
     var rate: Float?,
+
+    @Column(nullable = false)
     var min: Int?,
+
+    @Column(nullable = false)
     var max: Int?,
+
+    @Column(nullable = false)
     var description: String?,
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     var status: BasicStatus?,
+
+    @Column(nullable = false)
     var originalRate: Float?
 ): BasicTimeEntity()
