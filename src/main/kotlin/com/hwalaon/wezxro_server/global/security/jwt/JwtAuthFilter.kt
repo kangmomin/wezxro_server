22bec558c66292aae5f31c09405cac18369bdae8
koldin108902 <alex108902@naver.com>
2024-02-18 16:15:49 +0900

    Fix: 인증 단계에서 principal을 넘겨줌 userEmail을 String으로 넘겨줬는데 디버깅 해보면 타고들어가서 어떤 필터에서 UserDetails로 래핑하려 해서 에러가 찾기 어려운 위치에서 났었음

--- src/main/kotlin/com/hwalaon/wezxro_server/global/security/jwt/JwtAuthFilter.kt
+++ src/main/kotlin/com/hwalaon/wezxro_server/global/security/jwt/JwtAuthFilter.kt
@@ -1,6 +1,7 @@
 package com.hwalaon.wezxro_server.global.security.jwt
 
 import com.hwalaon.wezxro_server.domain.account.exception.AccountNotFoundException
+import com.hwalaon.wezxro_server.global.security.principal.PrincipalDetailsService
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
@@ -9,7 +10,8 @@ import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.filter.OncePerRequestFilter
 
 class JwtAuthFilter(
-    private val jwtParser: JwtParser
+    private val jwtParser: JwtParser,
+    private val principalDetailsService: PrincipalDetailsService,
 ) : OncePerRequestFilter() {
     override fun doFilterInternal(
         request: HttpServletRequest,
@@ -20,8 +22,10 @@ class JwtAuthFilter(
             val accessToken = jwtParser.parseAccessToken(request)
             if (!accessToken.isNullOrBlank()) {
                 val userEmail = jwtParser.authentication(accessToken, true)
+                val userDetails = principalDetailsService.loadUserByUsername(userEmail)
+
                 val securityContext = SecurityContextHolder.getContext()
-                securityContext.authentication = UsernamePasswordAuthenticationToken(userEmail, "", ArrayList())
+                securityContext.authentication = UsernamePasswordAuthenticationToken(userDetails, "", ArrayList())
             }
 
             filterChain.doFilter(request, response)
