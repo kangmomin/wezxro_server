72c6dfe1eca28098e76962e8a857d8392bf53f49
kangmomin <alex108902@naver.com>
2024-02-19 21:50:03 +0900

    Feat: 어노테이션 Valid

--- src/main/kotlin/com/hwalaon/wezxro_server/global/common/client/controller/request/AddClientRequest.kt
+++ src/main/kotlin/com/hwalaon/wezxro_server/global/common/client/controller/request/AddClientRequest.kt
@@ -1,5 +1,9 @@
 package com.hwalaon.wezxro_server.global.common.client.controller.request
 
+import jakarta.validation.constraints.NotEmpty
+
 data class AddClientRequest (
+
+    @NotEmpty
     val domain: String
 )
