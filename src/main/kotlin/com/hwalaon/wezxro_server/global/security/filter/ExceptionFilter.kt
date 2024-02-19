09c5ef16827197b4130fc736f5ff7aefe4743d18
kangmomin <alex108902@naver.com>
2024-02-19 21:50:34 +0900

    Feat: 404 페이지 핸들링

--- src/main/kotlin/com/hwalaon/wezxro_server/global/security/filter/ExceptionFilter.kt
+++ src/main/kotlin/com/hwalaon/wezxro_server/global/security/filter/ExceptionFilter.kt
@@ -3,7 +3,7 @@ package com.hwalaon.wezxro_server.global.security.filter
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.hwalaon.wezxro_server.global.common.basic.exception.BasicException
 import com.hwalaon.wezxro_server.global.common.basic.exception.ErrorCode
-import com.hwalaon.wezxro_server.global.common.basic.response.MsgResponse
+import com.hwalaon.wezxro_server.global.common.basic.response.BasicResponse
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
@@ -26,11 +26,22 @@ class ExceptionFilter: OncePerRequestFilter() {
     }
 
     private fun exceptionToResponse(errorCode: ErrorCode, response: HttpServletResponse) {
-        response.status = errorCode.status.value()
-        response.contentType = "application/json"
-        response.characterEncoding = "utf-8"
-        val errorResponse = MsgResponse(errorCode.msg)
-        val errorResponseToJson = ObjectMapper().writeValueAsString(errorResponse)
-        response.writer.write(errorResponseToJson)
+        val errorResponse = BasicResponse.error(errorCode)
+
+        // 상태 코드 설정
+        response.status = errorResponse.statusCode.value()
+        response.contentType = "application/json;charset=UTF-8"
+
+        // 헤더 설정
+        errorResponse.headers.forEach { name, values ->
+            values.forEach { value ->
+                response.addHeader(name, value)
+            }
+        }
+
+        // 본문 설정
+        errorResponse.body?.let {
+            response.writer.write(ObjectMapper().writeValueAsString(it))
+        }
     }
 }
