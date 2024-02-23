952103c3c2328252bdc2fa006f3a9ba978f451e9
kangmomin <alex108902@naver.com>
2024-02-23 21:55:18 +0900

    Refector: accountDetail static rate

--- src/main/kotlin/com/hwalaon/wezxro_server/domain/account/controller/response/AccountDetailResponse.kt
+++ src/main/kotlin/com/hwalaon/wezxro_server/domain/account/controller/response/AccountDetailResponse.kt
@@ -4,20 +4,22 @@ import com.hwalaon.wezxro_server.domain.account.model.Account
 import com.hwalaon.wezxro_server.global.common.basic.constant.BasicStatus
 
 data class AccountDetailResponse (
-    var userId: Int?,
-    var name: String?,
-    var email: String?,
-    var money: Double?,
-    var status: BasicStatus?
+    val userId: Int,
+    val name: String,
+    val email: String,
+    val money: Double,
+    val status: BasicStatus,
+    val staticRate: Long,
 ) {
     companion object {
         fun fromDomain(domain: Account) =
             AccountDetailResponse(
-                domain.userId,
-                domain.name,
-                domain.email,
-                domain.money,
-                domain.status
+                domain.userId!!,
+                domain.name!!,
+                domain.email!!,
+                domain.money!!,
+                domain.status!!,
+                domain.staticRate!!
             )
     }
 }
