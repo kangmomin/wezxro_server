642525ebd065f0bf22a5f25892930e3b822f0eef
kangmomin <alex108902@naver.com>
2024-02-29 04:02:56 +0900

    Fix: Validation 활성화 및 설정한 메시지가 뜨도록 nullable 변수로 수정

--- src/main/kotlin/com/hwalaon/wezxro_server/domain/account/controller/request/LoginRequest.kt
+++ src/main/kotlin/com/hwalaon/wezxro_server/domain/account/controller/request/LoginRequest.kt
@@ -1,15 +1,19 @@
 package com.hwalaon.wezxro_server.domain.account.controller.request
 
+import jakarta.validation.Valid
+import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.NotNull
 import java.util.*
 
 data class LoginRequest (
-    @NotEmpty
-    val key: UUID,
+    @field:NotNull(message = "클라이언트 키가 없습니다. 관리자에게 문의해주세요.")
+    val key: UUID?,
 
-    @NotEmpty
-    val email: String,
+    @field:NotEmpty(message = "이메일은 필수 입력 값입니다.")
+    @field:Email(message = "이메일 서식이 아닙니다. 이메일을 제출해주세요.")
+    val email: String?,
 
-    @NotEmpty
-    val password: String
+    @field:NotEmpty(message = "비밀번호는 필수 입력 값입니다.")
+    val password: String?
 )
