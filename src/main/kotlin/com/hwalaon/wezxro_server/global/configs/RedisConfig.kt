ec971d621e1a5f26635199e30cb6ed0bfe6f0637
koldin108902 <alex108902@naver.com>
2024-03-19 11:10:17 +0900

    Fix: indexed 추가 후 ttl 만료시에도 데이터 보존 이슈 수정
    indexed는 data redis에서 자체 제공하는 기능이라 삭제 시에는 동작하지 않음. 해서 KeySpaceNotification을 활성화 함으로써 이 문제를 해결

--- src/main/kotlin/com/hwalaon/wezxro_server/global/configs/RedisConfig.kt
+++ src/main/kotlin/com/hwalaon/wezxro_server/global/configs/RedisConfig.kt
@@ -5,12 +5,14 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisKeyValueAdapter
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 
 
 @Configuration
-@EnableRedisRepositories
+@EnableRedisRepositories(
+    enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
 class RedisConfig(
     @Value("\${spring.data.redis.host}")
     private val host: String,
