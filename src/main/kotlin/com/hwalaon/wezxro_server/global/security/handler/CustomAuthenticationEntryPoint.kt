d6881dbad263efd965c682446e1b1fdb3f2c08f3
kangmomin <alex108902@naver.com>
2024-04-05 04:11:08 +0900

    Refector: import문 최적화

--- src/main/kotlin/com/hwalaon/wezxro_server/global/security/handler/CustomAuthenticationEntryPoint.kt
+++ src/main/kotlin/com/hwalaon/wezxro_server/global/security/handler/CustomAuthenticationEntryPoint.kt
@@ -1,13 +1,9 @@
 package com.hwalaon.wezxro_server.global.security.handler
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.hwalaon.wezxro_server.global.common.basic.exception.ErrorCode
-import com.hwalaon.wezxro_server.global.common.basic.response.BasicResponse
-import com.hwalaon.wezxro_server.global.common.exception.UnAuthorizedException
 import com.hwalaon.wezxro_server.global.security.exception.SecurityUnAuthorizedException
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
-import org.springframework.http.MediaType
 import org.springframework.security.core.AuthenticationException
 import org.springframework.security.web.AuthenticationEntryPoint
 
