5f5baf98557c790838e850d5808d3db3bf61d0b3
kangmomin <alex108902@naver.com>
2024-04-19 21:11:14 +0900

    Fix: 최소 충전 금액 수정

--- src/main/kotlin/com/hwalaon/wezxro_server/domain/deposit/controller/request/AddDepositRequest.kt
+++ src/main/kotlin/com/hwalaon/wezxro_server/domain/deposit/controller/request/AddDepositRequest.kt
@@ -3,12 +3,13 @@ package com.hwalaon.wezxro_server.domain.deposit.controller.request
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.hwalaon.wezxro_server.domain.deposit.model.Deposit
 import com.hwalaon.wezxro_server.domain.deposit.model.constant.DepositType
+import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.NotEmpty
 import jakarta.validation.constraints.NotNull
 
 class AddDepositRequest (
     @field: NotNull(message = "결제 금액은 비어있을 수 없습니다.")
-//    @field: Min(message = "충전은 1000원부터 가능합니다.", value = 1000)
+    @field: Min(message = "충전은 1000원부터 가능합니다.", value = 1000)
     @JsonProperty("payamount")
     var amount: Long?,
 
