35f48619c5b827fac198338050228409d065b609
koldin108902 <alex108902@naver.com>
2024-04-22 21:07:49 +0900

    Feat: BasicException 위에서 돌아가도록 수정

--- src/main/kotlin/com/hwalaon/wezxro_server/global/security/exception/BasicSecurityException.kt
+++ src/main/kotlin/com/hwalaon/wezxro_server/global/security/exception/BasicSecurityException.kt
@@ -1,11 +1,12 @@
 package com.hwalaon.wezxro_server.global.security.exception
 
+import com.hwalaon.wezxro_server.global.common.basic.exception.BasicException
 import com.hwalaon.wezxro_server.global.common.basic.exception.ErrorCode
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 
 open class BasicSecurityException (
-    open val errorCode: ErrorCode,
+    override val errorCode: ErrorCode,
     open val res: HttpServletResponse,
     open val req: HttpServletRequest
-): RuntimeException()
+): BasicException(errorCode)
