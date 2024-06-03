bcdd28f57d3c5036a647627e5022b9fd8ecbbea0
kangmomin <alex108902@naver.com>
2024-06-03 20:07:56 +0900

    Feat: MASTER role 추가

--- migration/240603-enum-update.sql
+++ migration/240603-enum-update.sql
@@ -1,7 +1,7 @@
 ALTER TABLE account DROP CONSTRAINT account_role_check;
 ALTER TABLE account
     ADD CONSTRAINT account_role_check
-        CHECK (role IN ('USER', 'ADMIN', 'DEMO'));
+        CHECK (role IN ('USER', 'ADMIN', 'DEMO', 'MASTER'));
 ALTER TABLE deposit DROP CONSTRAINT deposit_status_check;
 ALTER TABLE deposit
     ADD CONSTRAINT deposit_status_check
