7bc02c1414f8a1f27304e0c6c1257e4697c577ba
kangmomin <alex108902@naver.com>
2024-06-07 21:02:37 +0900

    Fix: local 프로필일 때만 .env 읽어오기

--- src/main/kotlin/com/hwalaon/wezxro_server/WezxroServerApplication.kt
+++ src/main/kotlin/com/hwalaon/wezxro_server/WezxroServerApplication.kt
@@ -3,6 +3,7 @@ package com.hwalaon.wezxro_server
 import io.github.cdimascio.dotenv.Dotenv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Profile
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 
 @SpringBootApplication
@@ -12,11 +13,19 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 ])
 class WezxroServerApplication
 
-fun main(args: Array<String>) {
+@Profile("local")
+fun initDotenv() {
 	val dotenv = Dotenv.configure().load()
 
 	dotenv.entries().forEach { entry ->
 		System.setProperty(entry.key, entry.value)
+	}
+}
+
+fun main(args: Array<String>) {
+	// local 프로필이면 .env 파일을 읽어옴
+	if ((System.getProperty("spring.profiles.active") ?: "default") == "local") {
+		initDotenv()
 	}
 
 	runApplication<WezxroServerApplication>(*args)
