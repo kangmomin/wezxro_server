32b908ae5c7fade763d2011c7f44c5646ca50508
koldin108902 <alex108902@naver.com>
2024-06-15 08:26:39 +0900

    Feat: valid 필드 한정 적용

--- src/main/kotlin/com/hwalaon/wezxro_server/domain/account/controller/request/UpdateMoneyRequest.kt
+++ src/main/kotlin/com/hwalaon/wezxro_server/domain/account/controller/request/UpdateMoneyRequest.kt
@@ -4,12 +4,12 @@ import jakarta.validation.constraints.NotEmpty
 import jakarta.validation.constraints.NotNull
 
 class UpdateMoneyRequest (
-    @NotNull(message = "타겟 유저의 아이디가 없습니다.")
+    @field: NotNull(message = "타겟 유저의 아이디가 없습니다.")
     val userId: Long?,
 
-    @NotNull(message = "추가 금액이 비어있습니다.")
+    @field: NotNull(message = "추가 금액이 비어있습니다.")
     val balance: Double?,
 
-    @NotEmpty(message = "비밀번호가 비어있습니다.")
+    @field: NotEmpty(message = "비밀번호가 비어있습니다.")
     val password: String?
 )
